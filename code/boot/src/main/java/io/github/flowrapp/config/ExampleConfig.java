b42dd56247f8258dd0a03684643d8e159fdecd5c
Sergio Bernal Parrondo <sergiobpa@ext.inditex.com>
2025-07-23 23:21:38 +0200

    feat: jackson modules

--- code/boot/src/main/java/io/github/flowrapp/config/ExampleConfig.java
+++ code/boot/src/main/java/io/github/flowrapp/config/ExampleConfig.java
@@ -1,8 +1,21 @@
 package io.github.flowrapp.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.blackbird.BlackbirdModule;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ExampleConfig {
+
+  @Bean
+  @Primary
+  ObjectMapper jacksonMapper() {
+    return new ObjectMapper()
+        .registerModule(new JavaTimeModule())
+        .registerModule(new BlackbirdModule());
+  }
 
 }
