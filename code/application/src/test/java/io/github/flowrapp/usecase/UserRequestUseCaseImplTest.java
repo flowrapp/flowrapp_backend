8f8172c879b154ced413dd431a4d44661a85d3cd
Sergio Bernal Parrondo <sergiobpa@ext.inditex.com>
2025-07-23 12:32:00 +0200

    feat: added tests

---
+++ code/application/src/test/java/io/github/flowrapp/usecase/UserRequestUseCaseImplTest.java
@@ -0,0 +1,57 @@
+package io.github.flowrapp.usecase;
+
+import io.github.flowrapp.exception.FunctionalException;
+import io.github.flowrapp.model.User;
+import io.github.flowrapp.model.UserRequest;
+import io.github.flowrapp.port.output.UserRepositoryOutput;
+import org.instancio.junit.InstancioExtension;
+import org.instancio.junit.InstancioSource;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@ExtendWith({MockitoExtension.class, InstancioExtension.class})
+class UserRequestUseCaseImplTest {
+
+    @Mock
+    private UserRepositoryOutput userRepositoryOutput;
+
+    @InjectMocks
+    private UserRequestUseCaseImpl userRequestUseCase;
+
+    @ParameterizedTest
+    @InstancioSource
+    void findUser_returnsUser_whenFound(UserRequest userRequest, User user) {
+        // GIVEN
+        when(userRepositoryOutput.findUserByName(userRequest.name()))
+                .thenReturn(Optional.of(user));
+
+        // WHEN
+        var result = userRequestUseCase.findUser(userRequest);
+
+        // THEN
+        assertThat(result)
+                .isNotNull()
+                .isEqualTo(user);
+    }
+
+    @ParameterizedTest
+    @InstancioSource
+    void findUser_throwsException_whenNotFound(UserRequest userRequest) {
+        // GIVEN
+        when(userRepositoryOutput.findUserByName(userRequest.name()))
+                .thenReturn(Optional.empty());
+
+        // WHEN / THEN
+        assertThrows(FunctionalException.class, () -> userRequestUseCase.findUser(userRequest));
+    }
+
+}
