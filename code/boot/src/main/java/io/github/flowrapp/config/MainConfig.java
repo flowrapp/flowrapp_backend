558cd57533a839819f41f156ac0cee5f2ee7f057
Sergio Bernal Parrondo <sergiobpa@ext.inditex.com>
2025-07-24 09:08:57 +0200

    feat: improved jackson module registration

--- code/boot/src/main/java/io/github/flowrapp/config/MainConfig.java
+++ code/boot/src/main/java/io/github/flowrapp/config/MainConfig.java
@@ -1,21 +1,22 @@
 package io.github.flowrapp.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.blackbird.BlackbirdModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
-public class ExampleConfig {
+public class MainConfig {
 
   @Bean
-  @Primary
-  ObjectMapper jacksonMapper() {
-    return new ObjectMapper()
-        .registerModule(new JavaTimeModule())
-        .registerModule(new BlackbirdModule());
+  Module javaTimeModule() {
+    return new JavaTimeModule();
+  }
+
+  @Bean
+  Module blackbirdModule() {
+    return new BlackbirdModule();
   }
 
 }
