3c675b3a013ebd0feeba0e0e48081b858b4f4304
Sergio Bernal Parrondo <sergiobpa@ext.inditex.com>
2025-08-22 00:48:32 +0200

    fix: key

--- code/infrastructure/jpa-business-bbdd/src/main/java/io/github/flowrapp/infrastructure/jpa/businessbd/repository/ReportJpaRepository.java
+++ code/infrastructure/jpa-business-bbdd/src/main/java/io/github/flowrapp/infrastructure/jpa/businessbd/repository/ReportJpaRepository.java
@@ -1,11 +1,12 @@
 package io.github.flowrapp.infrastructure.jpa.businessbd.repository;
 
 import io.github.flowrapp.infrastructure.jpa.businessbd.entity.ReportEntity;
+import io.github.flowrapp.infrastructure.jpa.businessbd.entity.ReportIdEntity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReportJpaRepository extends JpaRepository<ReportEntity, Integer>, QuerydslPredicateExecutor<ReportEntity> {
+public interface ReportJpaRepository extends JpaRepository<ReportEntity, ReportIdEntity>, QuerydslPredicateExecutor<ReportEntity> {
 }
