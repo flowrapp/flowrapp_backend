765470985ac9b2d068b33dc16d1d4e4d4570a8c8
divios <serberpar@gmail.com>
2025-08-29 00:17:35 +0200

    feat: added config

---
+++ code/infrastructure/rest-main-api/src/main/java/io/github/flowrapp/infrastructure/input/rest/mainapi/security/config/OauthConfig.java
@@ -0,0 +1,43 @@
+package io.github.flowrapp.infrastructure.input.rest.mainapi.security.config;
+
+import java.time.Duration;
+import java.util.List;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import org.kohsuke.github.GitHubBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration(proxyBeanMethods = false)
+public class OauthConfig {
+
+  /** Bean for verifying Google ID tokens using Google's public keys. */
+  @Bean
+  GoogleIdTokenVerifier googleApi(@Value("${app.security.oauth2.client.registration.google.client-id}") String clientId) {
+    return new GoogleIdTokenVerifier.Builder(
+        new NetHttpTransport(), GsonFactory.getDefaultInstance())
+            .setAudience(List.of(clientId))
+            .build();
+  }
+
+  @Bean
+  @Scope("prototype")
+  GitHubBuilder gitHubBuilder() {
+    return new GitHubBuilder();
+  }
+
+  @Bean(name = "githubOAuthRestTemplate")
+  RestTemplate restTemplate() {
+    return new RestTemplateBuilder()
+        .connectTimeout(Duration.ofSeconds(10))
+        .readTimeout(Duration.ofSeconds(10))
+        .build();
+  }
+
+}
