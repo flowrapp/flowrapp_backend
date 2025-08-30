490262f263e7681c244f757b2d438b64260046ab
Sevwee <sevwee1@gmail.com>
2025-08-30 18:13:16 +0200

    feat: fix integration tests

--- code/boot/src/test/resources/scripts/invitation_test_data.sql
+++ code/boot/src/test/resources/scripts/invitation_test_data.sql
@@ -1,5 +1,5 @@
-INSERT INTO flowrapp_management.business (name, owner_id, longitude, latitude, area, timezone_offset, created_at)
-VALUES ('Accept Invitation Business', 1, 0.0, 0.0, 100.0, 'Europe/Madrid', NOW());
+INSERT INTO flowrapp_management.business (name, address, town, city, country, owner_id, longitude, latitude, area, timezone_offset, created_at)
+VALUES ('Accept Invitation Business', 'carretera 1', 'Zahora', 'Cádiz', 'España', 1, 0.0, 0.0, 100.0, 'Europe/Madrid', NOW());
 
 INSERT INTO flowrapp_management.invitations (invited, invited_by, business_id, token, role, status)
 VALUES ((SELECT id from flowrapp_management.users where mail = 'test@test.com'),
